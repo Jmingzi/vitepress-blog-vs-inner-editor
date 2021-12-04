f7d9eacde3df92a736bc9e6c193b987afa4f4e34
yangming <yangming@shinemo.com>
2021-12-04 22:13:34 +0800

    build

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080'
-    : 'http://1.117.74.21:3000/demo/vitepress-blog-vs-inner-editor/dist'
+    : 'https://iming.work/demo/vitepress-blog-vs-inner-editor/dist'
 }
