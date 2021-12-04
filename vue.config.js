450691b38bcb40e7e14abbfd3e1d80b805937e60
yangming <yangming@shinemo.com>
2021-12-04 22:40:35 +0800

    fix: upload image

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'development'
     ? 'http://localhost:8080'
-    : 'https://iming.work/demo/vitepress-blog-vs-inner-editor/dist'
+    : 'https://iming.work/demo/vitepress-blog-vs-inner-editor/dist',
+  productionSourceMap: false
 }
